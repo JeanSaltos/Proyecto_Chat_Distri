cca9e6e3cf499dfc0cba735377d192bafc1e24d2
jeans <jmsaltos1@espe.edu.ec>
2025-11-09 15:25:37 -0500

    Sexta Parte: Aplicación

---
+++ src/main/java/com/chat/servidorchat/ServiorChatApplication.java
@@ -0,0 +1,13 @@
+package com.chat.servidorchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiorChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiorChatApplication.class, args);
+    }
+
+}
