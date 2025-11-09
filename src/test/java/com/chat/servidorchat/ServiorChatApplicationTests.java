fe02bfdfef30571699a443cc545694b67cd313e6
TupacVelasquez <tyvelasquez@espe.edu.ec>
2025-11-09 15:33:15 -0500

    Novena Parte: Directorio Test

---
+++ src/test/java/com/chat/servidorchat/ServiorChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chat.servidorchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiorChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
